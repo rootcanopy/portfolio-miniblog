10b2c47c03009078994c4d9fd5af34e2e56369be
rootcanopy <dave.trees201@gmail.com>
2020-04-29 00:12:08 +0200

    blog views and urls

---
+++ blog/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+
+class CommentForm(forms.Forms):
+    author = forms.Charfield(
+        max_length=60,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Your Name'
+        })
+    )
+    body = forms.Charfield(widget=forms.Textarea(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Leave a comment!'
+        })
+    )

b3f02c5791d9ebea9239b8824c0c5c33b04e8f2a
rootcanopy <dave.trees201@gmail.com>
2020-04-28 04:07:29 +0200

    to start building the app that showcases projects

--- portfolio_miniblog/settings.py
+++ portfolio_miniblog/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello_world',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +54,7 @@ ROOT_URLCONF = 'portfolio_miniblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["portfolio_miniblog/templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
